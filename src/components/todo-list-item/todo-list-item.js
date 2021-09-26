ed05dd343af8442e190e5e0287f1cb733da0cc67
alexander <urazaev.alexander@gmail.com>
2021-09-26 23:19:49 +0500

    delete funct has added

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -33,6 +33,12 @@ export default class TodoListItem extends Component {
                     <i className={'fa fa-exclamation'}/>
 
                 </button>
+
+                <button type={'button'}
+                        className={'btn btn-outline-danger btn-sm float-right'}
+                        onClick={onDeleted}>
+                    <i className="fa fa-trash-o"/>
+                </button>
             </span>
         )
 
